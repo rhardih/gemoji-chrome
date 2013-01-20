46fcc879fee147e757bfcba4fd9250ef4c45308f
René Hansen <renehh@gmail.com>
2013-01-20 18:56:49 +0100

    Initial commit.

---
+++ contentscript.js
@@ -0,0 +1,42 @@
+// Util function handily borrowed from
+// http://www.scottklarr.com/topic/425/how-to-insert-text-into-a-textarea-where-the-cursor-is/
+function insertAtCaret(text) {
+  var txtarea = document.activeElement;
+  console.log(txtarea);
+  var scrollPos = txtarea.scrollTop;
+  var strPos = 0;
+  var br = ((txtarea.selectionStart || txtarea.selectionStart == '0') ? 
+    "ff" : (document.selection ? "ie" : false ) );
+  if (br == "ie") { 
+    txtarea.focus();
+    var range = document.selection.createRange();
+    range.moveStart ('character', -txtarea.value.length);
+    strPos = range.text.length;
+  }
+  else if (br == "ff") strPos = txtarea.selectionStart;
+  
+  var front = (txtarea.value).substring(0,strPos);  
+  var back = (txtarea.value).substring(strPos,txtarea.value.length); 
+  txtarea.value=front+text+back;
+  strPos = strPos + text.length;
+  if (br == "ie") { 
+    txtarea.focus();
+    var range = document.selection.createRange();
+    range.moveStart ('character', -txtarea.value.length);
+    range.moveStart ('character', strPos);
+    range.moveEnd ('character', 0);
+    range.select();
+  }
+  else if (br == "ff") {
+    txtarea.selectionStart = strPos;
+    txtarea.selectionEnd = strPos;
+    txtarea.focus();
+  }
+  txtarea.scrollTop = scrollPos;
+}
+
+chrome.extension.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    insertAtCaret(request.message);
+  }
+);
