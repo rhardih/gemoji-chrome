dbcdb0232f58f3a5fc959531c92b8b3b2193d859
René Hansen <renehh@gmail.com>
2013-01-23 00:33:57 +0100

    Moved content of insertAtCaret into a try catch.
    Silences exception when no input-/textfield is selected.

--- contentscript.js
+++ contentscript.js
@@ -1,38 +1,40 @@
 // Util function handily borrowed from
 // http://www.scottklarr.com/topic/425/how-to-insert-text-into-a-textarea-where-the-cursor-is/
 function insertAtCaret(text) {
-  var txtarea = document.activeElement;
+  try {
+    var txtarea = document.activeElement;
 
-  var scrollPos = txtarea.scrollTop;
-  var strPos = 0;
-  var br = ((txtarea.selectionStart || txtarea.selectionStart == '0') ? 
-    "ff" : (document.selection ? "ie" : false ) );
-  if (br == "ie") { 
-    txtarea.focus();
-    var range = document.selection.createRange();
-    range.moveStart ('character', -txtarea.value.length);
-    strPos = range.text.length;
-  }
-  else if (br == "ff") strPos = txtarea.selectionStart;
-  
-  var front = (txtarea.value).substring(0,strPos);  
-  var back = (txtarea.value).substring(strPos,txtarea.value.length); 
-  txtarea.value=front+text+back;
-  strPos = strPos + text.length;
-  if (br == "ie") { 
-    txtarea.focus();
-    var range = document.selection.createRange();
-    range.moveStart ('character', -txtarea.value.length);
-    range.moveStart ('character', strPos);
-    range.moveEnd ('character', 0);
-    range.select();
-  }
-  else if (br == "ff") {
-    txtarea.selectionStart = strPos;
-    txtarea.selectionEnd = strPos;
-    txtarea.focus();
-  }
-  txtarea.scrollTop = scrollPos;
+    var scrollPos = txtarea.scrollTop;
+    var strPos = 0;
+    var br = ((txtarea.selectionStart || txtarea.selectionStart == '0') ? 
+      "ff" : (document.selection ? "ie" : false ) );
+    if (br == "ie") { 
+      txtarea.focus();
+      var range = document.selection.createRange();
+      range.moveStart ('character', -txtarea.value.length);
+      strPos = range.text.length;
+    }
+    else if (br == "ff") strPos = txtarea.selectionStart;
+    
+    var front = (txtarea.value).substring(0,strPos);  
+    var back = (txtarea.value).substring(strPos,txtarea.value.length); 
+    txtarea.value=front+text+back;
+    strPos = strPos + text.length;
+    if (br == "ie") { 
+      txtarea.focus();
+      var range = document.selection.createRange();
+      range.moveStart ('character', -txtarea.value.length);
+      range.moveStart ('character', strPos);
+      range.moveEnd ('character', 0);
+      range.select();
+    }
+    else if (br == "ff") {
+      txtarea.selectionStart = strPos;
+      txtarea.selectionEnd = strPos;
+      txtarea.focus();
+    }
+    txtarea.scrollTop = scrollPos;
+  } catch(e) {}
 }
 
 chrome.extension.onMessage.addListener(
