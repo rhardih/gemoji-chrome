1c3d800c2ac6ea46507c20e929de29aaa9aa84cd
René Hansen <renehh@gmail.com>
2013-01-23 00:16:11 +0100

    Major refactor.
    Optimised for speed, so the popup loads quicker. Now only loads the
    first 100 images right away and waits with the rest until the dom has
    loaded.
    
    Also simplified the markup, so images are no longer wrapped in a div.

--- popup.js
+++ popup.js
@@ -1,63 +1,83 @@
-function create_emoji_div(image_name) {
-  var image_node = document.createElement("img");
-  image_node.src = "emoji/" + image_name;
+/**
+ * Loads <count> images from the globale images
+ * array, starting at <offset> and inserts them
+ * into the <target> element.
+ */
+function load_images(offset, count, target) {
+  for(var i = offset; i < count + offset; i++) {
+    var image_node = document.createElement("img");
+    var name = images[i].name;
+    image_node.src = "emoji/" + name + ".png";
+    image_node.className = "emoji";
 
-  var div = document.createElement("div");
-  div.className = "emoji";
-  div.dataset.name = image_name.match(/(.*)\.[^.]+$/)[1];
-  div.appendChild(image_node);
-
-  var name_elm = document.getElementById("name");
-  div.onmouseover = function() {
-    name_elm.innerText = ":" + div.dataset.name + ":";
-  }
-
-  return div;
+    images[i].element = image_node;
+    target.appendChild(image_node);
+  };
 };
+
+/**
+ * Runs through all the emoji and filters
+ * out those that do not match <query>.
+ */
+function filter_emojis(query) {
+  for(var i = 0; i < images.length; i++) {
+    if(images[i].name.indexOf(query.toLowerCase()) !== -1) {
+      images[i].element.style.display = "block";
+    } else {
+      images[i].element.style.display = "none";
+    }
+  };
+};
+
+var immediately_loaded = document.createElement("div");
+immediately_loaded.className = "emojis";
+
+var remaining = document.createElement("div");
+remaining.className = "emojis";
+
+// Fill only the first 100 images.
+// This improves the load time of the popup
+load_images(0, 100, immediately_loaded);
 
 window.onload = function() {
   var emojis = document.getElementById("emojis");
+  var highlight = document.getElementById("highlight");
+
+  // Add the 100 first images
+  emojis.appendChild(immediately_loaded);
+
+  // Add the rest of the images
+  load_images(100, images.length - 100, remaining);
+  emojis.appendChild(remaining);
+
+  // Setup shortcode highlight on hover
   images.forEach(function(image) {
-    emoji_div = create_emoji_div(image);
-    emojis.appendChild(emoji_div);
+    image.element.onmouseover = function() {
+      highlight.innerText = ":" + image.name + ":";
+    }
   });
 
-  var name = document.getElementById("name");
-
-  var emoji_divs = emojis.getElementsByTagName("div");
-  function filter_emojis(query) {
-    console.log(query);
-    var name, i, div;
-    for (i = 0; i < emoji_divs.length; i++) {
-      div = emoji_divs[i]; 
-      name = div.dataset.name;
-      if(name.indexOf(query.toLowerCase()) !== -1) {
-        div.style.display = "block";
-      } else {
-        div.style.display = "none";
-      }
-    }
-  }
-
+  // Setup search
   var search = document.getElementById("search");
-  search.onkeydown = function() {
+  search.onkeyup = function() {
     filter_emojis(search.value);
   };
 
-  for (var i = 0; i < emoji_divs.length; i++) {
-    emoji_divs[i].onclick = function() {
-      var div = this;
-      chrome.tabs.getSelected(null, function(tab) {
-        chrome.tabs.sendMessage(tab.id, { message: ":" + div.dataset.name + ":" });
-      });
-      self.close();
-    }
-  }
-
+  // Handle clicking the "x" to cancel current search
   search.onclick = function() {
     if(search.value === "") {
       filter_emojis("");
     }
   }
+
+  // Setup handler for clicking on emojis
+  images.forEach(function(image) {
+    image.element.onclick = function() {
+      chrome.tabs.getSelected(null, function(tab) {
+        chrome.tabs.sendMessage(tab.id, { message: ":" + image.name + ":" });
+      });
+      self.close();
+    }
+  });
 };
 
