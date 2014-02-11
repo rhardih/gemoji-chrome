3c01ce35fb2ffd79538d7b835630e613ca187009
René Hansen <renehh@gmail.com>
2014-02-12 00:59:59 +0100

    Updated package script

--- package.sh
+++ package.sh
@@ -9,19 +9,23 @@ pushd gemoji
 git checkout-index -f -a --prefix=../tmp/gemoji/
 popd
 
+zip -r tmp/gemoji-chrome.zip stylesheets
+zip tmp/gemoji-chrome.zip images/emoji-*.png
+
 pushd tmp
 
+zip -d gemoji-chrome.zip .gitignore
 zip -d gemoji-chrome.zip .gitmodules
-zip -d gemoji-chrome.zip emoji
+zip -d gemoji-chrome.zip images/emoji
 zip -d gemoji-chrome.zip gemoji/
 zip -d gemoji-chrome.zip README.md
-zip -d gemoji-chrome.zip create_images_js.sh
+zip -d gemoji-chrome.zip create_emojis_js.sh
 zip -d gemoji-chrome.zip package.sh
-zip -d gemoji-chrome.zip demo.gif
-
-pushd gemoji/images
-zip -r ../../gemoji-chrome.zip emoji
-popd
+zip -d gemoji-chrome.zip prepare_images.sh
+zip -d gemoji-chrome.zip assets/demo.gif
+zip -d gemoji-chrome.zip assets/chrome-store/*
+zip -d gemoji-chrome.zip sass/*
+zip -d gemoji-chrome.zip config.rb
 
 popd
 
